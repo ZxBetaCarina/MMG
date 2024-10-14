8c38a91dddfb84a01caee4bb57ddefa0606f7cd2
ZxDelt <jayantcarina@gmail.com>
2024-10-14 08:46:03 +0530

    some changes

---
+++ Assets/_this/Scripts/DockManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DockManager : MonoBehaviour
+{
+    [SerializeField] Image _dock_bg;
+    [SerializeField] List<Sprite> _dock_bg_sprite_list;
+    [SerializeField] List<Image> _dock_active_button_list;
+    [SerializeField] List<Button> _dock_inactive_button_list;
+    [SerializeField] Transform _dock_screens_parent;
+    [SerializeField] float _fade_duration;
+
+    private int _last_dock;
+
+    public void ActivateDockButtonAndScreen(int p_index)
+    {
+        _last_dock = p_index;
+
+        // Active buttons
+        for (int i = 0; i < _dock_active_button_list.Count; i++)
+        {
+            int t_index = i;
+            if (i != p_index)
+            {
+                AppearanceManager._AM.Fade_game_object_out(_dock_active_button_list[i].gameObject, _fade_duration, AnimationDirection.Down, () => _dock_active_button_list[t_index].enabled = false);
+            }
+        }
+
+        _dock_active_button_list[p_index].enabled = true;
+        AppearanceManager._AM.Fade_game_object_in(_dock_active_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Up);
+
+        // Dock background
+        _dock_bg.sprite = _dock_bg_sprite_list[p_index];
+
+        // Inactive buttons
+        for (int i = 0; i < _dock_inactive_button_list.Count; i++)
+        {
+            int t_index = i;
+            if (i != p_index && !_dock_inactive_button_list[t_index].interactable)
+            {
+                _dock_inactive_button_list[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                AppearanceManager._AM.Fade_game_object_in(_dock_inactive_button_list[i].transform.GetChild(0).gameObject, _fade_duration, AnimationDirection.Up, () =>
+                {
+                    _dock_inactive_button_list[t_index].interactable = true;
+                });
+            }
+        }
+
+        _dock_inactive_button_list[p_index].transform.GetChild(0).GetComponent<Image>().enabled = false;
+        _dock_inactive_button_list[p_index].interactable = false;
+        AppearanceManager._AM.Fade_game_object_out(_dock_inactive_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Down, null);
+
+        // Dock screens
+        for (int i = 0; i < _dock_screens_parent.childCount; i++)
+        {
+            GameObject t_selected_screen_object = _dock_screens_parent.GetChild(i).gameObject;
+            if (i != p_index)
+            {
+                AppearanceManager._AM.Fade_game_object_out(t_selected_screen_object, _fade_duration, AnimationDirection.Down, () => t_selected_screen_object.SetActive(false));
+            }
+        }
+
+        StartCoroutine(Cor_ActivateDockButtonAndScreen(p_index));
+    }
+
+    private IEnumerator Cor_ActivateDockButtonAndScreen(int p_index)
+    {
+        yield return new WaitForSeconds(_fade_duration);
+        GameObject t_selected_screen_object = _dock_screens_parent.GetChild(p_index).gameObject;
+        t_selected_screen_object.SetActive(true);
+        AppearanceManager._AM.Fade_game_object_in(t_selected_screen_object, _fade_duration, AnimationDirection.Up);
+    }
+}
--- Assets/_this/Scripts/UIManagement.cs
+++ Assets/_this/Scripts/UIManagement.cs
@@ -17,11 +17,12 @@ public class UIManagement : MonoBehaviour
     [SerializeField] float _fade_duration, _splash_duration;
 
     [Header("Dock")]
-    [SerializeField] Image _dock_bg;
-    [SerializeField] List<Sprite> _dock_bg_sprite_list;
-    [SerializeField] List<Image> _dock_active_button_list;
-    [SerializeField] List<Button> _dock_inactive_button_list;
-    [SerializeField] Transform _dock_screens_parent;
+    [SerializeField] DockManager dockManager;  // Reference to the DockManager
+    // [SerializeField] Image _dock_bg;
+    // [SerializeField] List<Sprite> _dock_bg_sprite_list;
+    // [SerializeField] List<Image> _dock_active_button_list;
+    // [SerializeField] List<Button> _dock_inactive_button_list;
+    // [SerializeField] Transform _dock_screens_parent;
 
     [Header("QR TEMP")]
     [SerializeField] GameObject _qr_overlay_object;
@@ -48,6 +49,12 @@ public class UIManagement : MonoBehaviour
         {
             StartCoroutine(Cor_splash_out());
         });
+    }
+    
+
+    public void Button_Activate_Dock_Button_And_Screen(int p_index)
+    {
+        dockManager.ActivateDockButtonAndScreen(p_index);
     }
 
     IEnumerator Cor_splash_out()
@@ -98,65 +105,65 @@ public class UIManagement : MonoBehaviour
         AppearanceManager._AM.Fade_game_object_in(t_selected_screen_object, _fade_duration, AnimationDirection.Up);
     }
 
-    public void Button_Activate_Dock_Button_And_Screen(int p_index)
-    {
-        _last_dock = p_index;
-        //  active buttons
-        for (int i = 0; i < _dock_active_button_list.Count; i++)
-        {
-            int t_index = i;
-            if (i != p_index)
-            {
-                AppearanceManager._AM.Fade_game_object_out(_dock_active_button_list[i].gameObject, _fade_duration, AnimationDirection.Down, () => _dock_active_button_list[t_index].enabled = false);
-            }
-        }
+    // public void Button_Activate_Dock_Button_And_Screen(int p_index)
+    // {
+    //     _last_dock = p_index;
+    //     //  active buttons
+    //     for (int i = 0; i < _dock_active_button_list.Count; i++)
+    //     {
+    //         int t_index = i;
+    //         if (i != p_index)
+    //         {
+    //             AppearanceManager._AM.Fade_game_object_out(_dock_active_button_list[i].gameObject, _fade_duration, AnimationDirection.Down, () => _dock_active_button_list[t_index].enabled = false);
+    //         }
+    //     }
+    //
+    //     _dock_active_button_list[p_index].enabled = true;
+    //     AppearanceManager._AM.Fade_game_object_in(_dock_active_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Up);
+    //
+    //     //  dock bg
+    //     _dock_bg.sprite = _dock_bg_sprite_list[p_index];
+    //
+    //     //  inactive buttons
+    //     for (int i = 0; i < _dock_inactive_button_list.Count; i++)
+    //     {
+    //         int t_index = i;
+    //         if (i != p_index && !_dock_inactive_button_list[t_index].interactable)
+    //         {
+    //             _dock_inactive_button_list[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+    //             AppearanceManager._AM.Fade_game_object_in(_dock_inactive_button_list[i].transform.GetChild(0).gameObject, _fade_duration, AnimationDirection.Up, () =>
+    //             {
+    //                 _dock_inactive_button_list[t_index].interactable = true;
+    //             });
+    //         }
+    //     }
+    //
+    //     _dock_inactive_button_list[p_index].transform.GetChild(0).GetComponent<Image>().enabled = false;
+    //     _dock_inactive_button_list[p_index].interactable = false;
+    //     AppearanceManager._AM.Fade_game_object_out(_dock_inactive_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Down, () =>
+    //     {
+    //
+    //     });
+    //
+    //     //  dock screens
+    //     for (int i = 0; i < _dock_screens_parent.childCount; i++)
+    //     {
+    //         GameObject t_selected_screen_object = _dock_screens_parent.GetChild(i).gameObject;
+    //         if (i != p_index)
+    //         {
+    //             AppearanceManager._AM.Fade_game_object_out(t_selected_screen_object, _fade_duration, AnimationDirection.Down, () => t_selected_screen_object.SetActive(false));
+    //         }
+    //     }
+    //     StartCoroutine(Cor_activate_dock_button_and_screen(p_index));
+    // }
 
-        _dock_active_button_list[p_index].enabled = true;
-        AppearanceManager._AM.Fade_game_object_in(_dock_active_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Up);
-
-        //  dock bg
-        _dock_bg.sprite = _dock_bg_sprite_list[p_index];
-
-        //  inactive buttons
-        for (int i = 0; i < _dock_inactive_button_list.Count; i++)
-        {
-            int t_index = i;
-            if (i != p_index && !_dock_inactive_button_list[t_index].interactable)
-            {
-                _dock_inactive_button_list[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
-                AppearanceManager._AM.Fade_game_object_in(_dock_inactive_button_list[i].transform.GetChild(0).gameObject, _fade_duration, AnimationDirection.Up, () =>
-                {
-                    _dock_inactive_button_list[t_index].interactable = true;
-                });
-            }
-        }
-
-        _dock_inactive_button_list[p_index].transform.GetChild(0).GetComponent<Image>().enabled = false;
-        _dock_inactive_button_list[p_index].interactable = false;
-        AppearanceManager._AM.Fade_game_object_out(_dock_inactive_button_list[p_index].gameObject, _fade_duration, AnimationDirection.Down, () =>
-        {
-
-        });
-
-        //  dock screens
-        for (int i = 0; i < _dock_screens_parent.childCount; i++)
-        {
-            GameObject t_selected_screen_object = _dock_screens_parent.GetChild(i).gameObject;
-            if (i != p_index)
-            {
-                AppearanceManager._AM.Fade_game_object_out(t_selected_screen_object, _fade_duration, AnimationDirection.Down, () => t_selected_screen_object.SetActive(false));
-            }
-        }
-        StartCoroutine(Cor_activate_dock_button_and_screen(p_index));
-    }
-
-    IEnumerator Cor_activate_dock_button_and_screen(int p_index)
-    {
-        yield return new WaitForSeconds(_fade_duration);
-        GameObject t_selected_screen_object = _dock_screens_parent.GetChild(p_index).gameObject;
-        t_selected_screen_object.SetActive(true);
-        AppearanceManager._AM.Fade_game_object_in(t_selected_screen_object, _fade_duration, AnimationDirection.Up);
-    }
+    // IEnumerator Cor_activate_dock_button_and_screen(int p_index)
+    // {
+    //     yield return new WaitForSeconds(_fade_duration);
+    //     GameObject t_selected_screen_object = _dock_screens_parent.GetChild(p_index).gameObject;
+    //     t_selected_screen_object.SetActive(true);
+    //     AppearanceManager._AM.Fade_game_object_in(t_selected_screen_object, _fade_duration, AnimationDirection.Up);
+    // }
 
     public void Button_QR_Overlay()
     {

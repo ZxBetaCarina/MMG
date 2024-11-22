2ecd05a45fdca5eb6b6116a8d672ca28f6400e9b
YashCarina <YASHAGRAWAL@CARINASOFTLABS.IN>
2024-11-22 16:12:14 +0530

    gqerg

--- Assets/_this/Scripts/UI/Refer.cs
+++ Assets/_this/Scripts/UI/Refer.cs
@@ -8,7 +8,13 @@ public class Refer : MonoBehaviour
     [SerializeField] private TMP_Text referCode;
     [SerializeField] private Button copy;
     
+    private string referralCode;
     
+    private void Start()
+    {
+        GenerateReferralCode(); // Generate the referral code on startup
+        UpdateUI();
+    }
     private void OnEnable()
     {
         copy.onClick.AddListener(OnCopy);
@@ -18,8 +24,26 @@ public class Refer : MonoBehaviour
     {
         copy.onClick.RemoveListener(OnCopy);
     }
+    private void GenerateReferralCode()
+    {
+        const int codeLength = 8;
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder(codeLength);
+        for (int i = 0; i < codeLength; i++)
+        {
+            sb.Append(chars[Random.Range(0, chars.Length)]);
+        }
+
+        referralCode = sb.ToString();
+    }
+    private void UpdateUI()
+    {
+        referCode.text = referralCode; // Display the generated referral code
+    }
     private void OnCopy()
     {
-        
+        GUIUtility.systemCopyBuffer = referralCode;
+        Debug.Log("Referral code copied to clipboard: " + referralCode);
     }
 }
